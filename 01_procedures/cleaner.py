aa7f1d83cc340f9001af5909b2e639297b85a488
Maksim Zholudev <maximzms@gmail.com>
2023-06-12 10:42:49 +0300

    Add proc implementation

--- 01_procedures/cleaner.py
+++ 01_procedures/cleaner.py
@@ -1,2 +1,85 @@
+from enum import Enum
+from dataclasses import dataclass
+from math import sin, cos, radians
+
+
+class Device(Enum):
+    WATER = "water"
+    SOAP = "soap"
+    BRUSH = "brush"
+
+
+@dataclass
+class Robot:
+    x: int
+    y: int
+    angle: int
+    device: Device
+
+
 def run_cleaner(commands: list[str]) -> list[str]:
-    return []
+    robot = Robot(0, 0, 0, Device.WATER)
+    output = list[str]()
+    for command in commands:
+        robot, out = run_command(robot, command)
+        output.append(out)
+    return output
+
+def run_command(robot: Robot, command: str) -> tuple[Robot, str]:
+    action, args = parse_command(command)
+    match action:
+        case "move":
+            assert len(args) == 1
+            return move(robot, int(args[0]))
+        case "turn":
+            assert len(args) == 1
+            return turn(robot, int(args[0]))
+        case "set":
+            assert len(args) == 1
+            return set_state(robot, Device(args[0]))
+        case "start":
+            assert len(args) == 0
+            return start(robot)
+        case "stop":
+            assert len(args) == 0
+            return stop(robot)
+        case _:
+            assert False
+
+def parse_command(command: str) -> tuple[str, list[str]]:
+    words = command.split()
+    return words[0], words[1:]
+
+def move(robot: Robot, distance: int) -> tuple[Robot, str]:
+    angle_radians = radians(robot.angle)
+    new_robot = Robot(
+        round(robot.x + distance * cos(angle_radians)),
+        round(robot.y + distance * sin(angle_radians)),
+        robot.angle,
+        robot.device,
+    )
+    return new_robot, f"POS {new_robot.x}, {new_robot.y}"
+
+def turn(robot: Robot, delta: int) -> tuple[Robot, str]:
+    new_robot = Robot(
+        robot.x,
+        robot.y,
+        (robot.angle + delta) % 360,
+        robot.device,
+    )
+    return new_robot, f"ANGLE {new_robot.angle}"
+
+def set_state(robot: Robot, device: Device) -> tuple[Robot, str]:
+    new_robot = Robot(
+        robot.x,
+        robot.y,
+        robot.angle,
+        device,
+    )
+    return new_robot, f"STATE {new_robot.device.value}"
+
+def start(robot: Robot) -> tuple[Robot, str]:
+    return robot, f"START WITH {robot.device.value}"
+
+def stop(robot: Robot) -> tuple[Robot, str]:
+    return robot, f"STOP"
--- 01_procedures/test_cleaner.py
+++ 01_procedures/test_cleaner.py
@@ -1,6 +1,6 @@
 import unittest
 
-from cleaner import run_cleaner
+from cleaner import *
 
 
 class Test_run_cleaner(unittest.TestCase):
@@ -11,6 +11,166 @@ class Test_run_cleaner(unittest.TestCase):
         self.assertEqual(
             output,
             [])
+        
+    def test_example(self):
+        commands = [
+            "move 100",
+            "turn -90",
+            "set soap",
+            "start",
+            "move 50",
+            "stop",
+        ]
+        output = run_cleaner(commands)
+        self.assertEqual(
+            output,
+            [
+                "POS 100, 0",
+                "ANGLE 270",
+                "STATE soap",
+                "START WITH soap",
+                "POS 100, -50",
+                "STOP",
+            ])
+        
+    def test_square(self):
+        commands = [
+            "turn 90",
+            "set brush",
+            "start",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "stop",
+            "turn -90",
+            "set soap",
+            "start",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "stop",
+            "turn -90",
+            "set water",
+            "start",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "turn -90",
+            "move 100",
+            "stop",
+        ]
+        output = run_cleaner(commands)
+        self.assertEqual(
+            output,
+            [
+                "ANGLE 90",
+                "STATE brush",
+                "START WITH brush",
+                "POS 0, 100",
+                "ANGLE 0",
+                "POS 100, 100",
+                "ANGLE 270",
+                "POS 100, 0",
+                "ANGLE 180",
+                "POS 0, 0",
+                "STOP",
+                "ANGLE 90",
+                "STATE soap",
+                "START WITH soap",
+                "POS 0, 100",
+                "ANGLE 0",
+                "POS 100, 100",
+                "ANGLE 270",
+                "POS 100, 0",
+                "ANGLE 180",
+                "POS 0, 0",
+                "STOP",
+                "ANGLE 90",
+                "STATE water",
+                "START WITH water",
+                "POS 0, 100",
+                "ANGLE 0",
+                "POS 100, 100",
+                "ANGLE 270",
+                "POS 100, 0",
+                "ANGLE 180",
+                "POS 0, 0",
+                "STOP",
+            ])
+
+class Test_run_command(unittest.TestCase):
+
+    def test_move(self):
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.WATER), "move 100"),
+            (Robot(100, 0, 0, Device.WATER), "POS 100, 0"))
+        self.assertEqual(
+            run_command(Robot(20, 30, 0, Device.SOAP), "move 100"),
+            (Robot(120, 30, 0, Device.SOAP), "POS 120, 30"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 90, Device.WATER), "move 50"),
+            (Robot(0, 50, 90, Device.WATER), "POS 0, 50"))
+        self.assertEqual(
+            run_command(Robot(20, 30, 90, Device.SOAP), "move 50"),
+            (Robot(20, 80, 90, Device.SOAP), "POS 20, 80"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 30, Device.WATER), "move 100"),
+            (Robot(87, 50, 30, Device.WATER), "POS 87, 50"))
+        self.assertEqual(
+            run_command(Robot(20, 30, 30, Device.SOAP), "move 100"),
+            (Robot(107, 80, 30, Device.SOAP), "POS 107, 80"))
+        
+    def test_turn(self):
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.WATER), "turn 60"),
+            (Robot(0, 0, 60, Device.WATER), "ANGLE 60"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.SOAP), "turn -60"),
+            (Robot(0, 0, 300, Device.SOAP), "ANGLE 300"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 150, Device.WATER), "turn 60"),
+            (Robot(0, 0, 210, Device.WATER), "ANGLE 210"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 150, Device.SOAP), "turn -60"),
+            (Robot(0, 0, 90, Device.SOAP), "ANGLE 90"))
+        
+    def test_set_state(self):
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.WATER), "set water"),
+            (Robot(0, 0, 0, Device.WATER), "STATE water"))
+        self.assertEqual(
+            run_command(Robot(100, 200, 90, Device.WATER), "set soap"),
+            (Robot(100, 200, 90, Device.SOAP), "STATE soap"))
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.SOAP), "set brush"),
+            (Robot(0, 0, 0, Device.BRUSH), "STATE brush"))
+        
+    def test_start(self):
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.WATER), "start"),
+            (Robot(0, 0, 0, Device.WATER), "START WITH water"))
+        self.assertEqual(
+            run_command(Robot(100, 200, 90, Device.SOAP), "start"),
+            (Robot(100, 200, 90, Device.SOAP), "START WITH soap"))
+        
+    def test_stop(self):
+        self.assertEqual(
+            run_command(Robot(0, 0, 0, Device.WATER), "stop"),
+            (Robot(0, 0, 0, Device.WATER), "STOP"))
+        self.assertEqual(
+            run_command(Robot(100, 200, 90, Device.SOAP), "stop"),
+            (Robot(100, 200, 90, Device.SOAP), "STOP"))
+
 
 if __name__ == '__main__':
     unittest.main()
