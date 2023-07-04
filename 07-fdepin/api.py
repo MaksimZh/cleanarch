d63a2b8f5bab1fa82a63fd5643dd76edc5f2eb73
Maksim Zholudev <maximzms@gmail.com>
2023-07-04 11:47:50 +0300

    Add single function DI

--- 07-fdepin/api.py
+++ 07-fdepin/api.py
@@ -4,33 +4,56 @@ import pure_robot as pr
 
 InitFunc = Callable[[], pr.RobotState]
 CommandFunc = Callable[[tuple[str, ...], pr.RobotState, pr.TransferFunc], pr.RobotState]
-ControlFuncs = dict[str, CommandFunc]
 
-CleanerFuncs: ControlFuncs = {
-    "move": lambda args, state, transfer: pr.move(transfer, float(args[0]), state),
-    "turn": lambda args, state, transfer: pr.turn(transfer, float(args[0]), state),
-    "set": lambda args, state, transfer: pr.set_state(transfer, args[0], state),
-    "start": lambda args, state, transfer: pr.start(transfer, state),
-    "stop": lambda args, state, transfer: pr.stop(transfer, state),
-}
+def CleanerFunc(
+        command: tuple[str, ...],
+        state: pr.RobotState,
+        transfer: pr.TransferFunc
+        ) -> pr.RobotState:
+    action = command[0]
+    args = command[1:]
+    match action:
+        case "move":
+            return pr.move(transfer, float(args[0]), state)
+        case "turn":
+            return pr.turn(transfer, float(args[0]), state)
+        case "set":
+            return pr.set_state(transfer, args[0], state)
+        case "start":
+            return pr.start(transfer, state)
+        case "stop":
+            return pr.stop(transfer, state)
+        case _:
+            assert False
 
-SlowCleanerFuncs: ControlFuncs = {
-    "move": lambda args, state, transfer: pr.move(transfer, float(args[0]) / 2, state),
-    "turn": CleanerFuncs["turn"],
-    "set": CleanerFuncs["set"],
-    "start": CleanerFuncs["start"],
-    "stop": CleanerFuncs["stop"],
-}
+def SlowCleanerFunc(
+        command: tuple[str, ...],
+        state: pr.RobotState,
+        transfer: pr.TransferFunc
+        ) -> pr.RobotState:
+    action = command[0]
+    args = command[1:]
+    match action:
+        case "move":
+            return pr.move(transfer, float(args[0]) / 2, state)
+        case "turn":
+            return pr.turn(transfer, float(args[0]), state)
+        case "set":
+            return pr.set_state(transfer, args[0], state)
+        case "start":
+            return pr.start(transfer, state)
+        case "stop":
+            return pr.stop(transfer, state)
+        case _:
+            assert False
 
 def run(
         init: InitFunc,
         commands: list[str],
-        robot_funcs: ControlFuncs,
+        robot_func: CommandFunc,
         transfer: pr.TransferFunc
         ) -> None:
     state = init()
     for command in commands:
-        words = command.split()
-        action = words[0]
-        args = tuple(words[1:])
-        state = robot_funcs[action](args, state, transfer)
+        words = tuple(command.split())
+        state = robot_func(words, state, transfer)
---
+++ 08-fedpin1/pure_robot.py
@@ -0,0 +1,73 @@
+import math
+from dataclasses import dataclass
+from typing import Any, Callable
+from enum import Enum
+
+
+# режимы работы устройства очистки
+class DeviceState(Enum):
+    WATER = 1 # полив водой
+    SOAP  = 2 # полив мыльной пеной
+    BRUSH = 3 # чистка щётками
+
+
+@dataclass
+class RobotState:
+    x: float
+    y: float
+    angle: float
+    state: DeviceState
+
+Message = tuple[Any, ...]
+
+# взаимодействие с роботом вынесено в отдельную функцию
+TransferFunc = Callable[[Message], None]
+
+# перемещение
+def move(transfer: TransferFunc, dist: float, state: RobotState) -> RobotState:
+    angle_rads = state.angle * (math.pi/180.0)
+    new_state = RobotState(
+        state.x + dist * math.cos(angle_rads),
+        state.y + dist * math.sin(angle_rads),
+        state.angle,
+        state.state)
+    transfer(('POS(', new_state.x, ',', new_state.y, ')'))
+    return new_state
+
+# поворот
+def turn(transfer: TransferFunc, turn_angle: float, state: RobotState) -> RobotState:
+    new_state = RobotState(
+        state.x,
+        state.y,
+        state.angle + turn_angle,
+        state.state)
+    transfer(('ANGLE', new_state.angle))
+    return new_state
+
+# установка режима работы
+def set_state(transfer: TransferFunc, new_internal_state: str, state: RobotState) -> RobotState:
+    if new_internal_state=='water':
+        self_state = DeviceState.WATER
+    elif new_internal_state=='soap':
+        self_state = DeviceState.SOAP
+    elif new_internal_state=='brush':
+        self_state = DeviceState.BRUSH
+    else:
+        return state
+    new_state = RobotState(
+        state.x,
+        state.y,
+        state.angle,
+        self_state)
+    transfer(('STATE', self_state))
+    return new_state
+
+# начало чистки
+def start(transfer: TransferFunc, state: RobotState) -> RobotState:
+    transfer(('START WITH', state.state))
+    return state
+
+# конец чистки
+def stop(transfer: TransferFunc, state: RobotState) -> RobotState:
+    transfer(('STOP',))
+    return state
